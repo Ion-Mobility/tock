07496ca4c8b57c0b0c3f76e2e121c52f783216f7
Nguyen Truong <nguyenlkdn@gmail.com>
2023-03-20 09:41:01 +0700

    add wdt handler

--- chips/s32k14x/src/wdt.rs
+++ chips/s32k14x/src/wdt.rs
@@ -68,12 +68,6 @@ impl Wdt {
             enabled: Cell::new(false),
         }
     }
-    /// WDT Errata: §45.1.3
-    ///
-    /// When writing any of the PSEL, TBAN, EN, or MODE fields, must insert a
-    /// delay for synchronization to complete.
-    ///
-    /// Also handle the KEY for the caller since we're special casing this.
     fn write_cr(&self, counter: FieldValue<u32, Counter::Register>) {
         WDT_REGS.cnt.write(Counter::COUNT::UNLOCK);
         WDT_REGS.toval.write(Timout::TIMEOUT::UNLOCK);
@@ -84,65 +78,21 @@ impl Wdt {
                 + Control::WIN::CLEAR
                 + Control::UPDATE::SET,
         );
-        // When writing to the affected fields, the user must ensure a wait
-        // corresponding to 2 clock cycles of both the WDT peripheral bus clock
-        // and the selected WDT clock source.
-        //
-        // TODO: Actual math based on chosen clock, ASF does:
-        //       delay = div_ceil(sysclk_hz(), OSC_[chosen]_NOMINAL_HZ)
-        for _ in 0..10 {
+        for _ in 0..10000 {
             support::nop();
         }
-        support::nop();
-        support::nop();
     }
     fn start(&self, period: usize) {
         self.enabled.set(true);
-
-        // pm::enable_clock(Clock::PBD(PBDClock::WDT));
-
-        // Note: Must use this clock to allow deep sleep. If you leave the
-        // default RCSYS, then the watchdog simply will not fire if you enter
-        // deep sleep (despite §20.4.1's protestations to the contrary).
-        // This is lower power anyway, so take the win.
-        // self.select_clock(WdtClockSource::ClockOsc32);
-
-        // Choose the best period setting based on what was passed to `start()`
-        //
-        // §20.5.1.3 Configuring the WDT
-        //
-        // T_timeout = T_psel = 2^(PSEL + 1) / f_wdt_clk
-        //
-        // Period is in ms so use freq in khz for easy integer math
-        // let f_clk_khz: u64 = if WDT_REGS.cr.matches_all(Control::CSSEL::RCSYS) {
-        //     115
-        // } else {
-        //     // OSC32K
-        //     32
-        // };
-        // let mult: u64 = f_clk_khz * (period as u64);
-        // let scaler = log_base_two_u64(mult); // prefer rounding for longer WD (thus no -1)
-
-        // let control = Control::CEN::ClockEnable
-        //     + Control::PSEL.val(scaler)
-        //     + Control::FCD::DoNotRedoCalibration
-        //     + Control::DAR::DisableAfterReset
-        //     + Control::EN::Enable;
-        // self.write_cr(control);
     }
 
     fn stop(&self) {
         self.write_cr(Counter::COUNT::SET);
-
-        // pm::disable_clock(Clock::PBD(PBDClock::WDT));
-
-        // self.enabled.set(false);
+        self.enabled.set(false);
     }
 
     fn tickle(&self) {
-        // Need to write the WDTCLR bit twice for it to work
-        // WDT_REGS.clr.write(Clear::KEY::KEY1 + Clear::WDTCLR::SET);
-        // WDT_REGS.clr.write(Clear::KEY::KEY2 + Clear::WDTCLR::SET);
+        support::nop();
     }
 }
 
